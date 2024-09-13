1f766a8e1c6b51cc5894a3d39583584a1722ed33
Prj_Rocks <prajjaldhar41@gmail.com>
2024-09-13 10:45:25 +0530

    Time: 3 ms (39.06%) | Memory: 8.4 MB (17.11%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+         if (n == 0) {
+           return 1.0; // Anything raised to the power of 0 is 1.
+    }
+        return pow(x,n);
+    }
+};
