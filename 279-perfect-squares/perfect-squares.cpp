8780f2a1729c43336c1f3462abd53845a9621537
Prj_Rocks <prajjaldhar41@gmail.com>
2024-09-16 10:15:25 +0530

    Time: 74 ms (52.86%) | Memory: 11.1 MB (72.57%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int sol(int n)
+{
+        vector<int> dp(n+1,INT_MAX);
+        dp[0] = 0;
+    for(int j = 1;j<=n;j++)
+    {
+        int mini = INT_MAX;
+        for(int i = 1;i<=sqrt(j);i++)
+       {
+           int sq = i*i;
+           int curr = 1 + dp[j - sq];
+           mini = min(mini,curr);
+       }
+       dp[j] = mini;
+    }
+    return dp[n];
+}
+    int numSquares(int n) 
+    {
+       return sol(n);
+    }
+};
