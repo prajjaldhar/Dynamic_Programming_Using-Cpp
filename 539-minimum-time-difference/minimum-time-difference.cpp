c3a519fa16c81384e07c7e10e3bec12ffc812397
Prj_Rocks <prajjaldhar41@gmail.com>
2024-09-16 12:45:33 +0530

    Time: 22 ms (7.08%) | Memory: 18.3 MB (27.31%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.cpp
+++ 539-minimum-time-difference/minimum-time-difference.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
-   int timeConvertToMinutes(string time)
+   int convertstringtoint(string str)
     {
-        int hourToMinute = stoi(time.substr(0, 2)) * 60;
-        int minute = stoi(time.substr(3, 2));
-        return hourToMinute + minute;
+        string s1 = str.substr(0,2);
+        string s2= str.substr(3,2);
+        int num1 = stoi(s1)*60;
+        int num2= stoi(s2);
+        return num1+num2;
     }
 
-    int findMinDifference(vector<string>& timePoints) 
+    int findMinDifference(vector<string>& s) 
     {
-        // Sort the time points
-        sort(begin(timePoints), end(timePoints));
-
-        // Add the first time point again at the end to account for the circular nature of time
-        timePoints.push_back(timePoints[0]);
-
-        // Variable to store the minimum difference
-        int ans = INT_MAX, n = timePoints.size(), time_one, time_two;
-        
-        // Loop through the time points to find the minimum difference
-        for(int i = 0; i < n - 1; i++)
-        {
-            time_one = timeConvertToMinutes(timePoints[i]);
-            time_two = timeConvertToMinutes(timePoints[i + 1]);
-            
-            // Calculate the difference between consecutive time points
-            ans = min(ans, abs(time_one - time_two));
-            
-            // Calculate the difference considering the circular nature of time
-            ans = min(ans, abs(time_one - (1440 + time_two)));
-        }
+         sort(begin(s), end(s));
+          s.push_back(s[0]);
+          int ans = INT_MAX;
+          for(int i=0;i<s.size()-1;i++)
+          {
+            int  time_one = convertstringtoint(s[i]);
+             int time_two = convertstringtoint(s[i + 1]);
+             // Calculate the difference between consecutive time points
+             ans = min(ans, abs(time_one - time_two));  
+             // Calculate the difference considering the circular nature of time
+             ans = min(ans, abs(time_one - (1440 + time_two)));
+          }
         return ans;
     }
 };
