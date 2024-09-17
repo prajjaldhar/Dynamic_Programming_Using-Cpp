99bdfdcc822cf9aed476c4d466b235e07906dacd
Prj_Rocks <prajjaldhar41@gmail.com>
2024-09-17 11:57:29 +0530

    Time: 80 ms (48.76%) | Memory: 11.2 MB (72.49%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -9,7 +9,7 @@ public:
         int mini = INT_MAX;
         for(int i = 1;i<=sqrt(j);i++)
        {
-           int sq = i*i;
+           int sq = i*i; 
            int curr = 1 + dp[j - sq];
            mini = min(mini,curr);
        }
