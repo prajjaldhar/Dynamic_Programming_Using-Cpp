f183801f8eb11c638129a300c3f17aef508241bb
Prj_Rocks <prajjaldhar41@gmail.com>
2024-12-24 16:11:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (24.82%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
-   bool isPowerOfTwo(int n)
-    {
-        // Early return for non-positive numbers
-        if (n <= 0) return false;
-        // Count the number of 1s in the binary representation
-       if(n==1)
-           return true;
-        while (n%2==0) {
-            n = n / 2;
+    bool isPowerOfTwo(int n) {
+        if(n==0) return false;
+        while(n%2==0)
+        {
+            n/=2;
         }
-        // A number is a power of two if it has exactly one 1 bit in its binary representation
-        return n == 1;
+        return n==1;
     }
 };
