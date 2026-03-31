c425dd29836be84a69ae88b01026e15033eee47e
Prj_Rocks <prajjaldhar41@gmail.com>
2026-03-31 09:48:57 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (11.88%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-         map<int, bool> mp;
+         int n = nums.size();
 
-        // Step 1: store all elements
-        for (int num : nums) {
-            mp[num] = true;
-        }
+    int expected = n * (n + 1) / 2;
+    int actual = 0;
 
-        // Step 2: check from 0 to n
-        for (int i = 0; i <= nums.size(); i++) {
-            if (mp.count(i) == 0) {
-                return i;
-            }
-        }
-
-        return -1; // never reached
+    for (int num : nums) {
+        actual += num;
     }
+
+    return expected - actual;}
 };
