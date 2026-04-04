fb9819c11222f4c97e4b59a8f17e83a8cfe9df57
Prj_Rocks <prajjaldhar41@gmail.com>
2026-04-04 17:24:07 +0530

    Time: 0 ms (100.00%) | Memory: 55.1 MB (30.54%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-  int firstMissingPositive(vector<int>& nums) {
-    map<int, bool> mp;
-
-    // Store all elements
-    for (int num : nums) {
-        mp[num] = true;
-    }
-
-    // Check from 1 to n
-    
-    for (int i=1; i <= nums.size(); i++) {
-        if (mp.count(i) == 0) {
-            return i;
+    int firstMissingPositive(vector<int>& nums) {
+         int n = nums.size();
+        
+        // Step 1: Place each number in its correct index
+        for (int i = 0; i < n; i++) {
+            while (
+                nums[i] > 0 && nums[i] <= n &&
+                nums[nums[i] - 1] != nums[i]
+            ) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
         }
-    }
-        return nums.size() + 1;
+        
+        // Step 2: Find first index where mismatch occurs
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        
+        // Step 3: If all correct
+        return n + 1;
     }
 };
