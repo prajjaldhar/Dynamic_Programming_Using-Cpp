ad3b7c82c9252a8533c6e17e7d343ab33ed9db21
tech.prajjal <prajjaldhar41@gmail.com>
2026-06-25 13:24:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (92.25%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-         // Negative numbers and numbers ending with 0 (except 0 itself) are not palindromes.
-        if (x < 0 || (x % 10 == 0 && x != 0)) {
-            return false;
+        if((x<0 || x%10==0) && x!=0)
+          return false;
+        if(x==0)
+            return true;
+        long sum=0;
+        while(x)
+        {
+            sum=sum*10+x%10;
+            if(sum==x || x/10==sum)
+            {
+                return true;
+            }
+            x/=10;
         }
-
-        int reversed = 0;
-        while (x>reversed) {
-            reversed = reversed * 10 + x % 10;
-            x /= 10;
-        }
-
-        // For even-length numbers, x and reversed will be the same.
-        // For odd-length numbers, reversed will have an extra digit in the middle, which we can ignore.
-        return x == reversed || x == reversed/10;
+        return false;
     }
 };
