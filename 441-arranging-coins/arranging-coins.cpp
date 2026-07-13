22740d1721c4632e96c990500d94649ab54ac592
tech.prajjal <prajjaldhar41@gmail.com>
2026-07-13 12:45:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (37.07%) - LeetSync

--- 441-arranging-coins/arranging-coins.cpp
+++ 441-arranging-coins/arranging-coins.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-              int rows = 0;
-
-        for (int i = 1; n >= i; i++) {
-            n -= i;
-            rows++;
-        }
-
-        return rows;
+             return (-1LL+sqrt(1LL+8LL*n))/2LL;
     }
 };
